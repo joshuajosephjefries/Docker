7243afe1734cab01f27c80bed4a12d8d934ba36a
joshuajosephjefries <79646955+joshuajosephjefries@users.noreply.github.com>
2022-04-19 16:38:15 +0530

    Created app.py

---
+++ Project1/app.py
@@ -0,0 +1,24 @@
+import time 
+import redis 
+import flask 
+
+app = flask.Flask(__name__)
+cache = redis.Redis(host='redis', port=6379)
+
+def get_hit_count():
+    retries = 10
+    while True:
+        try:
+            return cache.incr('hits')
+        except redis.exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise exc
+            retries -= 1
+            time.sleep(0.5)
+
+@app.route('/')
+def hello():
+    count = get_hit_count()
+    return 'Hello World! I have been seen {} times.\n'.format(count)
+    return "Hi. My name is Joseph Jefries and I am learning DevOps"
+    return "createdBy = joshuajosephjefries"
